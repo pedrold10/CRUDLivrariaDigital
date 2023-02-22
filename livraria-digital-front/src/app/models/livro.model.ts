b2e79cf54427f2a8251e5b367bfd6e7c80dcbd8a
Pedro Gabriel <58262161+pedrold10@users.noreply.github.com>
2023-02-22 08:23:01 -0300

    Adicionando model e service

---
+++ livraria-digital-front/src/app/models/livro.model.ts
@@ -0,0 +1,11 @@
+export interface Livro{
+  id: string;
+  titulo: string;
+  subtitulo: string;
+  resumo: string;
+  quantidadePaginas: number;
+  dataPublicacao: Date;
+  editora: string;
+  edicao: number;
+  autor: string;
+}
