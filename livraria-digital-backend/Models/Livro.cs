bfa321508f2e03df10404f1a293b84cd7e96de6d
Pedro Gabriel <58262161+pedrold10@users.noreply.github.com>
2023-02-22 22:32:06 -0300

    Ajustes estéticos finais

--- livraria-digital-backend/Models/Livro.cs
+++ livraria-digital-backend/Models/Livro.cs
@@ -2,14 +2,20 @@ using System.ComponentModel.DataAnnotations;
 namespace livraria_digital_backend.Models{
     public class Livro {
         public Guid Id { get; set; }
+        [StringLength(100)]
         public string Titulo { get; set; }
+        [StringLength(100)]
         public string? Subtitulo { get; set; }
+        [StringLength(500)]
         public string? Resumo { get; set; }
         [Range(0, 1000)]
         public int QuantidadePaginas { get; set; }
         public DateTime DataPublicacao { get; set; }
+        [StringLength(150)]
         public string Editora { get; set; }
+        [Range(1, 99)]
         public int? Edicao { get; set; }
+        [StringLength(150)]
         public string Autor { get; set; }
     }
 }
